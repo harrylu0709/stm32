7ab4af354b2aee4c2933d765bdb4dcedc3b074c7
harrylu0709 <134115124+harrylu0709@users.noreply.github.com>
2024-09-28 16:04:30 +0800

    implment GPIO simulating I2C sending data from master to slave

---
+++ GPIO_I2C/Debug/Src/main.d
@@ -0,0 +1,6 @@
+Src/main.o: ../Src/main.c D:/MCU/MCU1/GPIO_I2C/drivers/Inc/stm32f407xx.h \
+ D:/MCU/MCU1/GPIO_I2C/drivers/Inc/stm32f407xx_gpio_driver.h \
+ D:/MCU/MCU1/GPIO_I2C/drivers/Inc/stm32f407xx.h
+D:/MCU/MCU1/GPIO_I2C/drivers/Inc/stm32f407xx.h:
+D:/MCU/MCU1/GPIO_I2C/drivers/Inc/stm32f407xx_gpio_driver.h:
+D:/MCU/MCU1/GPIO_I2C/drivers/Inc/stm32f407xx.h:
---
+++ GPIO_I2C/Debug/drivers/Src/stm32f407xx_gpio_driver.d
@@ -0,0 +1,8 @@
+drivers/Src/stm32f407xx_gpio_driver.o: \
+ ../drivers/Src/stm32f407xx_gpio_driver.c \
+ D:/MCU/MCU1/GPIO_I2C/drivers/Inc/stm32f407xx_gpio_driver.h \
+ D:/MCU/MCU1/GPIO_I2C/drivers/Inc/stm32f407xx.h \
+ D:/MCU/MCU1/GPIO_I2C/drivers/Inc/stm32f407xx_gpio_driver.h
+D:/MCU/MCU1/GPIO_I2C/drivers/Inc/stm32f407xx_gpio_driver.h:
+D:/MCU/MCU1/GPIO_I2C/drivers/Inc/stm32f407xx.h:
+D:/MCU/MCU1/GPIO_I2C/drivers/Inc/stm32f407xx_gpio_driver.h:
